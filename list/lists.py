9d93cbcd3c52cc98d886d356fd157b6cfa984f26
thenatural86 <philip.smalls86@gmail.com>
2024-08-20 10:27:39 -0300

    extend method

--- list/lists.py
+++ list/lists.py
@@ -152,3 +152,18 @@ List.insert(3, 12)
 List.insert(0, 'Geeks')
 print("\nList after performing Insert Operation: ")
 print(List)
+
+# Python program to demonstrate
+# Addition of elements in a List
+
+# Creating a List
+List = [1, 2, 3, 4]
+print("Initial List: ")
+print(List)
+
+# Addition of multiple elements
+# to the List at the end
+# (using Extend Method)
+List.extend([8, 'Geeks', 'Always'])
+print("\nList after performing Extend Operation: ")
+print(List)
