6d94de0253f14fd86133a79871e3a9907e7dcb8a
thenatural86 <philip.smalls86@gmail.com>
2024-08-20 10:39:03 -0300

    slicing

--- list/lists.py
+++ list/lists.py
@@ -167,3 +167,145 @@ print(List)
 List.extend([8, 'Geeks', 'Always'])
 print("\nList after performing Extend Operation: ")
 print(List)
+
+
+# Reversing a list
+mylist = [1, 2, 3, 4, 5, 'Geek', 'Python']
+mylist.reverse()
+print(mylist)
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = list(reversed(my_list))
+print(reversed_list)
+
+# Python program to demonstrate
+# Removal of elements in a List
+
+# Creating a List
+List = [1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12]
+print("Initial List: ")
+print(List)
+
+# Removing elements from List
+# using Remove() method
+List.remove(5)
+List.remove(6)
+print("\nList after Removal of two elements: ")
+print(List)
+
+# Creating a List
+List = [1, 2, 3, 4, 5, 6,
+        7, 8, 9, 10, 11, 12]
+# Removing elements from List
+# using iterator method
+for i in range(1, 5):
+    List.remove(i)
+print("\nList after Removing a range of elements: ")
+print(List)
+
+List = [1, 2, 3, 4, 5]
+
+# Removing element from the
+# Set using the pop() method
+List.pop()
+print("\nList after popping an element: ")
+print(List)
+
+# Removing element at a
+# specific location from the
+# Set using the pop() method
+List.pop(2)
+print("\nList after popping a specific element: ")
+print(List)
+
+
+
+
+
+# UNDERSTANDING SLICING OF LISTS:
+
+# pr[0] accesses the first item, 2.
+# pr[-4] accesses the fourth item from the end, 5.
+# pr[2:] accesses [5, 7, 11, 13], a list of items from third to last.
+# pr[:4] accesses [2, 3, 5, 7], a list of items from first to fourth.
+# pr[2:4] accesses [5, 7], a list of items from third to fifth.
+# pr[1::2] accesses [3, 7, 13], alternate items, starting from the second item.
+
+# Python program to demonstrate
+# Removal of elements in a List
+
+# Creating a List
+List = ['G', 'E', 'E', 'K', 'S', 'F',
+        'O', 'R', 'G', 'E', 'E', 'K', 'S']
+print("Initial List: ")
+print(List)
+
+# Print elements of a range
+# using Slice operation
+Sliced_List = List[3:8]
+print("\nSlicing elements in a range 3-8: ")
+print(Sliced_List)
+
+# Print elements from a
+# pre-defined point to end
+Sliced_List = List[5:]
+print("\nElements sliced from 5th "
+      "element till the end: ")
+print(Sliced_List)
+
+# Printing elements from
+# beginning till end
+Sliced_List = List[:]
+print("\nPrinting all elements using slice operation: ")
+print(Sliced_List)
+
+
+# Creating a List
+List = ['G', 'E', 'E', 'K', 'S', 'F',
+        'O', 'R', 'G', 'E', 'E', 'K', 'S']
+print("Initial List: ")
+print(List)
+
+# Print elements from beginning
+# to a pre-defined point using Slice
+Sliced_List = List[:-6]
+print("\nElements sliced till 6th element from last: ")
+print(Sliced_List)
+
+# Print elements of a range
+# using negative index List slicing
+Sliced_List = List[-6:-1]
+print("\nElements sliced from index -6 to -1")
+print(Sliced_List)
+
+# Printing elements in reverse
+# using Slice operation
+Sliced_List = List[::-1]
+print("\nPrinting List in reverse: ")
+print(Sliced_List)
+
+
+# Creating a List
+List = ['G', 'E', 'E', 'K', 'S', 'F',
+        'O', 'R', 'G', 'E', 'E', 'K', 'S']
+print("Initial List: ")
+print(List)
+
+# Print elements from beginning
+# to a pre-defined point using Slice
+Sliced_List = List[:-6]
+print("\nElements sliced till 6th element from last: ")
+print(Sliced_List)
+
+# Print elements of a range
+# using negative index List slicing
+Sliced_List = List[-6:-1]
+print("\nElements sliced from index -6 to -1")
+print(Sliced_List)
+
+# Printing elements in reverse
+# using Slice operation
+Sliced_List = List[::-1]
+print("\nPrinting List in reverse: ")
+print(Sliced_List)
