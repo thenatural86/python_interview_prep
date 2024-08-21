f762e1af7597cd0f0c9457a7d751e6fb5f5831ed
thenatural86 <philip.smalls86@gmail.com>
2024-08-21 09:39:36 -0300

    explanatiomn

--- list/shuffle_array/shuffle_array.py
+++ list/shuffle_array/shuffle_array.py
@@ -1,10 +1,14 @@
 # You can solve this problem by iterating through the first half of the array (nums[:n]) and the second half of the array (nums[n:]), and then interleaving the elements from each half to create the desired output.
 
 def shuffle_array(nums, n):
+    # We initialize an empty list result 
     result = []
     
+    # loop through the range n, 
     for i in range(n):
+        # append elements from the first half (nums[i]) 
         result.append(nums[i])
+        # and the second half (nums[n+i])
         result.append(nums[n + i])    
     return result
 
