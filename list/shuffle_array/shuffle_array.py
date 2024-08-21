4d6258092400effb26971b659fa262f21bbb3fa3
thenatural86 <philip.smalls86@gmail.com>
2024-08-21 09:24:27 -0300

    shuffle array

---
+++ list/shuffle_array/shuffle_array.py
@@ -0,0 +1,1 @@
+# You can solve this problem by iterating through the first half of the array (nums[:n]) and the second half of the array (nums[n:]), and then interleaving the elements from each half to create the desired output.
