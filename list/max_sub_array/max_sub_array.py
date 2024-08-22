6f628fa4762d91aae6d62e2ca2797f43e4c86438
thenatural86 <philip.smalls86@gmail.com>
2024-08-22 11:25:27 -0300

    max sub array

---
+++ list/max_sub_array/max_sub_array.py
@@ -0,0 +1,36 @@
+# Given an integer array nums, find the 
+# subarray
+#  with the largest sum, and return its sum.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# Example 3:
+
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+def max_sub_array(nums):
+    # keeps track of the maximum sum found so far.
+    max_sum = nums[0]
+    # keeps track of the sum of the current subarray.
+    # current_sum = nums[0]
+    current_sum = 0
+    
+    for num in nums:
+        if current_sum < 0:
+            current_sum = 0
+        current_sum += num
+        max_sum = max(max_sum,current_sum)
+    return max_sum
