90d194f859e55f4c0952b6c947e740a94cc5abf1
thenatural86 <philip.smalls86@gmail.com>
2024-08-23 17:10:24 -0300

    solved3

--- list/max_sub_array/max_sub_array_blank.py
+++ list/max_sub_array/max_sub_array_blank.py
@@ -6,5 +6,5 @@ def max_sub_array(nums):
         if current_sum < 0:
             current_sum = 0
         current_sum += num
-        max_sum = max(max_sum, current_sum)    
+        max_sum = max(max_sum,current_sum)
     return max_sum
