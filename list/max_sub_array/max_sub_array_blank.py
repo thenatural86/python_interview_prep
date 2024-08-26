801545700291a64bff3ca9a0c6e352168610935d
thenatural86 <philip.smalls86@gmail.com>
2024-08-26 12:16:54 -0300

    more array practice

--- list/max_sub_array/max_sub_array_blank.py
+++ list/max_sub_array/max_sub_array_blank.py
@@ -1,2 +1,12 @@
 def max_sub_array(nums):
-   pass
+   max_sum = nums[0]
+   current_sum = 0
+   
+   for num in nums:
+      if current_sum < 0:
+         current_sum = 0
+      
+      current_sum += num
+      max_sum = max(max_sum, current_sum)
+      
+   return max_sum
