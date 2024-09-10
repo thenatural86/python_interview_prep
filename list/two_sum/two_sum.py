ca42d90618eb754bcfa1484026703683fc22594b
thenatural86 <philip.smalls86@gmail.com>
2024-09-10 18:37:57 -0300

    structure folders

--- list/two_sum/two_sum.py
+++ list/two_sum/two_sum.py
@@ -18,26 +18,30 @@
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 
+# create an empty dictionary to store the difference between the target and each element as we iterate through the array
+
+# iterate through nums array
+        # The enumerate() function is used to loop over a sequence (like a list, tuple, or string) while keeping track of both the index and the value of each element in the sequence.
+# initialize the difference to be target - num
+# if the difference is in the dictionary, we've found the two indices that sum up to the target
+# so we return the index from the dictionary and the current index.
+# If not, we add the current element and its index to the dictionary.
+
 class Solution(object):
     """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-    # create an empty dictionary to store the difference between the target and each element as we iterate through the array
+
     nums = [2,7,11,15]
     target = 9
-    
+
     def twoSum(self,nums,target):
         num_map = {}
-        # iterate through nums array
-        # The enumerate() function is used to loop over a sequence (like a list, tuple, or string) while keeping track of both the index and the value of each element in the sequence.
+
         for i, num in enumerate(nums):
-            # initialize the difference to be target - num
             diff = target - num
-            # if the difference is in the dictionary, we've found the two indices that sum up to the target
             if diff in num_map:
-                # so we return the index from the dictionary and the current index.
                 return [num_map[diff], i]
-            # If not, we add the current element and its index to the dictionary.
             num_map[num] = i
