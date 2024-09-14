7f6afacd35cdf3590ae58f4b48fec56c9a6fc340
thenatural86 <philip.smalls86@gmail.com>
2024-09-14 17:00:49 -0300

    practice

--- list/pascals_triangle/generate_blank.py
+++ list/pascals_triangle/generate_blank.py
@@ -4,7 +4,7 @@ def generate(numRows):
     for i in range(numRows):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i + 1][j]
-        triangle.append(row)
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle. append(row)
 
     return triangle
