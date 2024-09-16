4f8592b86d49d259cbf750aa2bbf6cee4090950f
thenatural86 <philip.smalls86@gmail.com>
2024-09-16 15:18:59 -0300

    daily practice

--- list/max_sub_array/max_sub_array_blank.py
+++ list/max_sub_array/max_sub_array_blank.py
@@ -1,9 +1,9 @@
 def max_sub_array(nums):
-   max_sum = nums[0]
-   current_sum = nums[0]
+    max_sum = nums[0]
+    current_sum = nums[0]
 
-   for num in nums[1:]:
-      current_sum = max(num, current_sum + num)
-      max_sum = max(max_sum, current_sum)
+    for num in nums:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
 
-   return max_sum
+    return max_sum
--- list/two_sum/two_sum_blank.py
+++ list/two_sum/two_sum_blank.py
@@ -1,8 +1,8 @@
 def two_sum(nums, target):
-   num_map = {}
+    num_map = {}
 
-   for i, num in enumerate(nums):
-      diff = target - num
-      if diff in num_map:
-         return [num_map[diff], i]
-      num_map[num] = i
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in num_map:
+            return [num_map[diff], i]
+        num_map[num] = i
