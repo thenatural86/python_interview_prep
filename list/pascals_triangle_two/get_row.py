9d9ad676c0f9ae5ff8bb1139cf8c86ace78dca4d
thenatural86 <philip.smalls86@gmail.com>
2024-09-19 17:13:40 -0300

    daily practice

--- list/pascals_triangle_two/get_row.py
+++ list/pascals_triangle_two/get_row.py
@@ -2,5 +2,5 @@ def getRow(rowIndex):
     row = [1]
 
     for i in range(1, rowIndex + 1):
-        row = [1] + [row[j] + row[j + 1] for j in range(len(row) - 1)] + [1]
+        row = [1] + [row[j + 1] + row[j] for j in range(len(row) - 1)] + [1]
     return row
