e7696721144b4c34c4d1f23bdc1f431ace5afab2
thenatural86 <philip.smalls86@gmail.com>
2024-09-21 18:02:41 -0300

    daily practice

--- list/can_place_flowers/can_place_flowers_blank.py
+++ list/can_place_flowers/can_place_flowers_blank.py
@@ -3,3 +3,6 @@ def canPlaceFlowers(flowerbed, n):
         if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
             flowerbed[i] = 1
             n -= 1
+            if n == 0:
+                return True
+    return n <= 0
