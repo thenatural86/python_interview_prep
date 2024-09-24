91d87db5ccb56432e139c5558538e106ba2be14c
thenatural86 <philip.smalls86@gmail.com>
2024-09-24 10:47:24 -0300

    daily practice

--- list/defuse_the_bomb/decrypt_blank.py
+++ list/defuse_the_bomb/decrypt_blank.py
@@ -1,14 +1,1 @@
 def decrypt(code, k):
-    n = len(code)
-    result = [0] * n
-
-    if k == 0:
-        return result
-
-    for i in range(n):
-        if k > 0:
-            result[i] = sum(code[(i + j) % n] for j in range(1, k +1 ))
-        else:
-            result[i] = sum(code[(i + j) % n] for j in range(k, 0))
-
-    return result
