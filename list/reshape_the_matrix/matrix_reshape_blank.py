6e6c843eb4cad8d2e55a84d873be83452f3976d9
thenatural86 <philip.smalls86@gmail.com>
2024-10-09 09:48:08 -0300

    daily practice

--- list/reshape_the_matrix/matrix_reshape_blank.py
+++ list/reshape_the_matrix/matrix_reshape_blank.py
@@ -1,1 +1,15 @@
 def matrixReshape(mat, r, c):
+    m = len(mat)
+    n = len(mat[0] if m > 0 else 0)
+
+    if m * n != r * c:
+        return mat
+
+    flat_list = [num for row in mat for num in row]
+
+    new_mat = []
+
+    for i in range(r):
+        new_mat.append(flat_list[i * c: (i + 1) * c])
+
+    return new_mat
