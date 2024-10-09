4a69bddd4d6fc024321ba697c99e864bda323127
thenatural86 <philip.smalls86@gmail.com>
2024-10-09 09:44:04 -0300

    daily practice

--- list/shuffle_array/shuffle_array.rb
+++
@@ -1,8 +0,0 @@
-def shuffle(nums, n)
-  result = []
-  (0...n).each do |i|
-    result << nums[i]      # Append x_i
-    result << nums[n + i]  # Append y_i
-  end
-  result
-end
--- list/shuffle_array/shuffle_array_blank.rb
+++
@@ -1,9 +0,0 @@
-def shuffle_array(nums, n):
-  result = {}
-
-  (0...n).each do |i|
-    result << nums[i]
-    result << nums[n + i]
-  end
-  return result
-end
