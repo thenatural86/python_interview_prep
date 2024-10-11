d2834ccec7d39698e6690d4a86e428169f4a1676
thenatural86 <philip.smalls86@gmail.com>
2024-10-11 14:38:20 -0300

    daily practice

--- list/longest_consecutive_sequence/longest_consecutive_blank.py
+++ list/longest_consecutive_sequence/longest_consecutive_blank.py
@@ -1,12 +1,1 @@
 def largest_consecutive(nums):
-    numSet = set(nums)
-    longest = 0
-
-    for n in nums:
-        if n - 1 not in numSet:
-            length = 0
-            while(n + length) in numSet:
-                length += 1
-            longest = max(length, longest)
-
-    return longest
