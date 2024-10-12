84eb5389904d9387f3e91988407ca166dcc750ab
thenatural86 <philip.smalls86@gmail.com>
2024-10-12 12:44:10 -0300

    daily practice

--- hashmaps/design_hash_map_blank.py
+++ hashmaps/design_hash_map_blank.py
@@ -1,5 +1,15 @@
 class ListNode:
+
     def __init__(self, key=-1, val=-1, next=None):
         self.key = key
         self.val = val
         self.next = next
+
+
+class MyHashMap(object):
+
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
