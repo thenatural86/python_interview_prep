aa9e64f0c4fdf34ac885c1a98dcaf9ed703ce3f0
thenatural86 <philip.smalls86@gmail.com>
2024-10-12 15:39:32 -0300

    daily practice

--- hashmaps/design_hash_map_blank.py
+++ hashmaps/design_hash_map_blank.py
@@ -1,6 +1,6 @@
 class ListNode:
 
-    def __init__(self, key=-1, val=-1, next=None):
+    def __init__(self, key=-1, val=-1, next= None):
         self.key = key
         self.val = val
         self.next = next
