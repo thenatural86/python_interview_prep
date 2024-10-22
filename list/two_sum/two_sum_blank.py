3ba107c6a92f2bae4bea32b1e10f69ba7b3a4394
thenatural86 <philip.smalls86@gmail.com>
2024-10-22 10:01:53 -0300

    daily practice

--- list/two_sum/two_sum_blank.py
+++ list/two_sum/two_sum_blank.py
@@ -1,1 +1,8 @@
 def two_sum(nums, target):
+    num_map = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if num in num_map:
+            return [num_map[diff], i]
+        num_map[num] = i
