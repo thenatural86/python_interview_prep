f9662837e63c53bfbf66efbd6deb5c60903a7bfe
thenatural86 <philip.smalls86@gmail.com>
2024-10-22 12:12:19 -0300

    daily practice

---
+++ hashmaps/design_hashmap/design_hash_map_blank.py
@@ -0,0 +1,45 @@
+class ListNode:
+
+    def __init__(self, key=-1, val=-1, next=None):
+        self.key = key
+        self.val = val
+        self.next = next
+
+
+class MyHashMap(object):
+
+    def __init__(self):
+        self.map = [ListNode() for i in range(1000)]
+
+    def hash(self, key):
+        return key % len(self.map)
+
+    def put(self, key, value):
+        cur = self.map[self.hash(key)]
+
+        while cur.next:
+            if cur.next.key == key:
+                cur.next.val = value
+                return
+            cur = cur.next
+
+        cur.next = ListNode(key, value)
+
+    def get(self, key):
+        cur = self.map[self.hash(key)].next
+
+        while cur:
+            if cur.key == key:
+                return cur.val
+            cur = cur.next
+
+        return -1
+
+    def remove(self, key):
+        cur = self.map[self.hash(key)]
+
+        while cur and cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
+                return
+            cur = cur.next
