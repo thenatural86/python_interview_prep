8004bb66462278970c2e91f323bb2dfe239286ba
thenatural86 <philip.smalls86@gmail.com>
2024-10-23 09:31:27 -0300

    daily practice

--- list/pascals_triangle/generate_blank.py
+++ list/pascals_triangle/generate_blank.py
@@ -1,1 +1,10 @@
 def generate(numRows):
+    triangle = []
+
+    for i in range(numRows):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i -1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+
+    return triangle
