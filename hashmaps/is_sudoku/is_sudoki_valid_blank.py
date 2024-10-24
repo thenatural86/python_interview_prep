e26e4b30ec190477a0c4eb89efa994e2776e9a22
thenatural86 <philip.smalls86@gmail.com>
2024-10-24 09:45:22 -0300

    daily practice

--- hashmaps/is_sudoku/is_sudoki_valid_blank.py
+++ hashmaps/is_sudoku/is_sudoki_valid_blank.py
@@ -2,3 +2,7 @@ def isSudoku(self,board):
     rows = [set() for _ in range(9)]
     cols = [set() for _ in range(9)]
     boxes = [set() for _ in range(9)]
+
+    for r in range(9):
+        for c in range(9):
+            num = board[r][c]
