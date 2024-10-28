459103d42bdee802e4c9952a9ab295dcf672880f
thenatural86 <philip.smalls86@gmail.com>
2024-10-28 15:42:59 -0300

    daily practice

---
+++ hashmaps/containes_dupes/contain_dupes.py
@@ -0,0 +1,8 @@
+def containsNearbyDuplicate(nums, k):
+    index_map = {}
+
+    for i, num in enumerate(nums):
+        if num in index_map and i - index_map[num] <= k:
+            return True
+        index_map[num] = i
+    return False
