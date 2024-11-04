73fb8ac6b157a421905435a57db2519782c22b71
thenatural86 <philip.smalls86@gmail.com>
2024-11-04 09:17:07 -0300

    daily practice

--- hashmaps/is_sudoku/is_sudoki_valid_blank.py
+++ hashmaps/is_sudoku/is_sudoki_valid_blank.py
@@ -10,10 +10,11 @@ def isSudoku(self,board):
             if num == '.':
                 continue
 
-            box_index = (r // 3) * 3 + 3 + c // 3
+            box_index = (r // 3) * 3 + c // 3
 
             if num in rows[r] or num in cols[c] or num in boxes[box_index]:
                 return False
+
             rows[r].add(num)
             cols[c].add(num)
             boxes[box_index].add(num)
