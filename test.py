7fcb4a3b5d5970efe29a3e88ea06aa06d92eb4d3
thenatural86 <philip.smalls86@gmail.com>
2024-11-07 10:50:59 -0300

    daily practice

--- test.py
+++
@@ -1,9 +0,0 @@
-def version_compare( version1, version2 ) :
-	#Insert your code here
-    if version1 == version2:
-        return 0
-    if version1 > version2:
-        return 1
-    if version1 < version2:
-        return -1
-    float(version2)
