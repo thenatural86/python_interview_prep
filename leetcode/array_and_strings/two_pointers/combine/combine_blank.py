a02ca4cb40e0b20111522e00f82c2ed4ee653f0b
thenatural86 <philip.smalls86@gmail.com>
2024-11-10 10:37:32 -0300

    daily practice

--- leetcode/array_and_strings/two_pointers/combine/combine_blank.py
+++ leetcode/array_and_strings/two_pointers/combine/combine_blank.py
@@ -1,5 +1,26 @@
 def combine(arr1, arr2):
 
+    i = j = 0
+    ans = []
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j
+
+    return ans
+
 
 
 arr1 = [1, 4, 7, 20]
