0d6ba8442c2fa3ab9d681e2609718dab81d6cd38
thenatural86 <philip.smalls86@gmail.com>
2024-11-10 10:27:54 -0300

    daily practice

--- leetcode/array_and_strings/two_pointers/check_target/check_target_blank.py
+++ leetcode/array_and_strings/two_pointers/check_target/check_target_blank.py
@@ -1,4 +1,16 @@
 def check_for_target(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        if nums[left] + nums[right] == target:
+            return True
+        if nums[left] + nums[right] > target:
+            right -= 1
+        if nums[left] + nums[right] < target:
+            left += 1
+
+    return False
 
 
 nums = [1, 2, 4, 6, 8, 9, 14, 15]
