bf4a43c064f525ddc4c976dd08ff574643ded13d
thenatural86 <philip.smalls86@gmail.com>
2024-11-10 10:44:44 -0300

    daily practice

--- leetcode/array_and_strings/two_pointers/is_palindrome/is_palindrome_blank.py
+++ leetcode/array_and_strings/two_pointers/is_palindrome/is_palindrome_blank.py
@@ -1,4 +1,16 @@
 def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
 
 
 print(is_palindrome("hello"))
