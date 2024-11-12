f17e0c19410f0f70fae9ed462875c4c0eb92a820
thenatural86 <philip.smalls86@gmail.com>
2024-11-12 11:46:16 -0300

    daily practice

--- leetcode/array_and_strings/sliding_window/find_length.py
+++ leetcode/array_and_strings/sliding_window/find_length.py
@@ -1,2 +1,18 @@
 def find_length(nums, k):
-    pass
+    left = curr = ans = 0
+
+    for right in range(len(nums)):
+        curr += nums[right]
+        while curr > k:
+            curr -= nums[left]
+            left += 1
+
+        ans = max(ans, right - left + 1)
+
+    return ans
+
+
+nums = [3, 1, 2, 7, 4, 2, 1, 1, 5]
+k = 8
+
+print(find_length(nums, k))
