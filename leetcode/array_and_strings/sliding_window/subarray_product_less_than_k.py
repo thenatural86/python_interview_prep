04574b63ff0416f0fe5b376d053bf56bd8bd9d2d
thenatural86 <philip.smalls86@gmail.com>
2024-11-12 15:32:28 -0300

    daily practice

--- leetcode/array_and_strings/sliding_window/subarray_product_less_than_k.py
+++ leetcode/array_and_strings/sliding_window/subarray_product_less_than_k.py
@@ -1,5 +1,20 @@
 def numSubarrayProductLessThanK(nums, k):
-    pass
+    if k <= 1:
+        return 0
+
+    ans = left = 0
+    curr = 1
+
+    for right in range(len(nums)):
+        curr *= nums[right]
+        while curr >= k:
+            curr //= nums[left]
+            left += 1
+
+        ans += right - left + 1
+
+    return ans
+
 
 
 nums = [10, 5, 2, 6]
