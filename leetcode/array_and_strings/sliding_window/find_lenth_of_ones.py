e5a498e44f5949f89f5aa6374c0e7afb4ff814d2
thenatural86 <philip.smalls86@gmail.com>
2024-11-12 12:11:35 -0300

    daily practice

--- leetcode/array_and_strings/sliding_window/find_lenth_of_ones.py
+++ leetcode/array_and_strings/sliding_window/find_lenth_of_ones.py
@@ -1,2 +1,12 @@
 def find_length(s):
-    pass
+    curr = 0
+
+    for right in range(len(s)):
+        if s[right] == '0':
+            curr += 1
+        while curr <= 1:
+            pass
+
+s = "1101100111"
+
+print(find_length(s))
