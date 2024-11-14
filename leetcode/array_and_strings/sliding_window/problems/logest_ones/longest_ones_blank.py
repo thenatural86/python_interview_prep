ddcae2d04d24d16c6ba824fba677031dcd42d38a
thenatural86 <philip.smalls86@gmail.com>
2024-11-14 11:21:52 -0300

    daily practice

--- leetcode/array_and_strings/sliding_window/problems/logest_ones/longest_ones_blank.py
+++ leetcode/array_and_strings/sliding_window/problems/logest_ones/longest_ones_blank.py
@@ -1,2 +1,10 @@
 def longestOnes(nums, k):
-    pass
+    left = 0
+
+    for right in range(len(nums)):
+        k -= 1 - nums[right]
+        if k < 0:
+            k += 1 - nums[left]
+            left += 1
+
+    return right - left + 1
