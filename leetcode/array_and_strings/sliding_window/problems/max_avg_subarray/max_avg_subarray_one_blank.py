e133c61789d6dd167abf508364aadc3a842f42a2
thenatural86 <philip.smalls86@gmail.com>
2024-11-15 13:38:58 -0300

    daily practice

--- leetcode/array_and_strings/sliding_window/problems/max_avg_subarray/max_avg_subarray_one_blank.py
+++ leetcode/array_and_strings/sliding_window/problems/max_avg_subarray/max_avg_subarray_one_blank.py
@@ -1,5 +1,20 @@
 def findMaxAverage(nums, k):
+    n = len(nums)
+    cur_sum = 0
 
+    for i in range(k):
+        cur_sum += nums[i]
+
+    max_avg = cur_sum / k
+
+    for i in range(k, n):
+        cur_sum += nums[i]
+        cur_sum -= nums[i - k]
+
+        avg = cur_sum / k
+        max_avg = max(max_avg, avg)
+
+    return max_avg
 
 
 nums = [1,12,-5,-6,50,3]
