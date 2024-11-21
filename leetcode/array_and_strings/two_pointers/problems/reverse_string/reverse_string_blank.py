1b7f86b0df08e6ab7b497b7371b6f59001c56df1
thenatural86 <philip.smalls86@gmail.com>
2024-11-21 16:17:12 -0500

    daily leetcode

--- leetcode/array_and_strings/two_pointers/problems/reverse_string/reverse_string_blank.py
+++ leetcode/array_and_strings/two_pointers/problems/reverse_string/reverse_string_blank.py
@@ -1,11 +1,12 @@
 def reverseString(s):
+
     left = 0
     right = len(s) - 1
 
     while left < right:
-        temp = s[left]
+        tmp = s[left]
         s[left] = s[right]
-        s[right] = temp
+        s[right] = tmp
 
         left += 1
         right -= 1
