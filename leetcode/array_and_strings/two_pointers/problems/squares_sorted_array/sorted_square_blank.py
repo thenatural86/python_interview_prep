5861ef983e1e7aa2b1ea61507453aba67d776f00
thenatural86 <philip.smalls86@gmail.com>
2024-11-21 16:32:26 -0500

    daily leetcode

--- leetcode/array_and_strings/two_pointers/problems/squares_sorted_array/sorted_square_blank.py
+++ leetcode/array_and_strings/two_pointers/problems/squares_sorted_array/sorted_square_blank.py
@@ -4,17 +4,17 @@ def sortedSquares(nums):
     left = 0
     right = n - 1
 
-    for i in range(n - 1, -1, -1):
+    for i in range(n):
         if abs(nums[left]) < abs(nums[right]):
             square = nums[right]
             right -= 1
         else:
             square = nums[left]
             left += 1
+
         result[i] = square * square
 
     return result
-
 
 
 nums = [-4,-1,0,3,10]
