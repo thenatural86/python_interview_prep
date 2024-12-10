04646c0656aaf9fd142f647cd84e51060ac33de7
thenatural86 <philip.smalls86@gmail.com>
2024-12-10 14:00:34 -0500

    leetcode practice

--- leetcode/array_and_strings/two_pointers/problems/reverse_string/reverse_string_blank.py
+++ leetcode/array_and_strings/two_pointers/problems/reverse_string/reverse_string_blank.py
@@ -1,16 +1,4 @@
 def reverseString(s):
-    left = 0
-    right = len(s) - 1
-
-    while left < right:
-        tmp = s[left]
-        s[left] = s[right]
-        s[right] = tmp
-
-        left += 1
-        right -= 1
-
-    return s
 
 
 s = ["h","e","l","l","o"]
