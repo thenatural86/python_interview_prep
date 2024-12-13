1b762f90724be36d5d5f428eed210207a79104f5
thenatural86 <philip.smalls86@gmail.com>
2024-12-13 18:01:02 -0500

    leetcode examples

---
+++ leetcode/array_and_strings/prefix_sum/examples/ways_split_array.py
@@ -0,0 +1,11 @@
+def waysToSplitArray(nums):
+    ans = left_section = 0
+    total = sum(nums)
+
+    for i in range(len(nums) - 1):
+        left_section += nums[i]
+        right_section = total - left_section
+        if left_section >= right_section:
+            ans += 1
+
+    return ans
