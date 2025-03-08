cc2c957a83d27b4209a4c9cd2b2f4e871fd48e95
thenatural86 <philip.smalls86@gmail.com>
2025-03-08 13:57:24 -0500

    user failed logins

--- mercury_interview_prep/data_fetching/user_auth.js
+++ mercury_interview_prep/data_fetching/user_auth.js
@@ -25,3 +25,23 @@ const users = [
     timestamp: '2025-03-01T10:20:00Z',
   },
 ]
+
+// Count the number of failed login attempts per user.
+const failedLogin = users.reduce((acc, person) => {
+  const { user, status } = person
+
+  // initialize user count if it doesn't exist
+  if (!acc[user]) {
+    acc[user] = 0
+  }
+
+  if (status === 'failed') {
+    acc[user] += 1
+  }
+  return acc
+}, {})
+
+console.log(failedLogin)
+// Identify users who had consecutive failed attempts before a successful login.
+
+// Find the most recent login attempt per user (by timestamp).
