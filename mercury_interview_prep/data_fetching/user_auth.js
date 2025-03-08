0f85f58d0a95ef4d5c69c3523d4f7e3b6005e6da
thenatural86 <philip.smalls86@gmail.com>
2025-03-08 13:33:17 -0500

    user authentication

---
+++ mercury_interview_prep/data_fetching/user_auth.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    user: 'alice@example.com',
+    status: 'success',
+    timestamp: '2025-03-01T10:00:00Z',
+  },
+  {
+    user: 'bob@example.com',
+    status: 'failed',
+    timestamp: '2025-03-01T10:05:00Z',
+  },
+  {
+    user: 'alice@example.com',
+    status: 'failed',
+    timestamp: '2025-03-01T10:10:00Z',
+  },
+  {
+    user: 'alice@example.com',
+    status: 'success',
+    timestamp: '2025-03-01T10:15:00Z',
+  },
+  {
+    user: 'bob@example.com',
+    status: 'success',
+    timestamp: '2025-03-01T10:20:00Z',
+  },
+]
