f2d188268578990e7331a88fa69c159e06219951
thenatural86 <philip.smalls86@gmail.com>
2025-03-09 13:44:06 -0400

    times items are purchased

--- mercury_interview_prep/data_fetching/more_customer_orders.js
+++ mercury_interview_prep/data_fetching/more_customer_orders.js
@@ -12,4 +12,16 @@ const completedOrdersRevenue = data
   .filter((order) => order.status !== 'pending')
   .reduce((acc, order) => acc + order.totalAmount, 0)
 
-console.log(completedOrdersRevenue)
+// console.log(completedOrdersRevenue)
+
+//count the number of times each product was purchased
+const timesItemPurchased = data.reduce((acc, order) => {
+  order.items.forEach(({ productId, quantity, name }) => {
+    if (!acc[productId]) {
+      acc[productId] = { name, quantity: 0 }
+    }
+    acc[productId].quantity += quantity
+  })
+  return acc
+}, {})
+console.log(timesItemPurchased)
