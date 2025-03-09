ce1ad2a282d5473fdf75f0e08c2cd9773b377e2b
thenatural86 <philip.smalls86@gmail.com>
2025-03-09 13:26:31 -0400

    completed order revenue

--- mercury_interview_prep/data_fetching/more_customer_orders.js
+++ mercury_interview_prep/data_fetching/more_customer_orders.js
@@ -4,4 +4,12 @@ import { data } from '../data.js'
 const pendingOrders = data.filter((order) => {
   return order.status === 'pending'
 })
-console.log(pendingOrders)
+// console.log(pendingOrders)
+
+// get the total revenue for all completed orders (shipped, delivered)
+
+const completedOrdersRevenue = data
+  .filter((order) => order.status !== 'pending')
+  .reduce((acc, order) => acc + order.totalAmount, 0)
+
+console.log(completedOrdersRevenue)
